4e4560772ab0271e9216670c2d912b22780c6ca4
Mathieu <bertrand.mathieu.74@gmail.com>
2025-04-02 08:32:58 +0200

    Update setup_with_ddcv.R

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -1,11 +1,11 @@
 library(datadrivencv)
 library(here)
 use_datadriven_cv(
-  full_name = "Nick Strayer",
-  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "nickstrayer.me/cv/",
-  source_location = "https://github.com/nstrayer/cv",
+  full_name = "Mathieu Bertrand",
+  data_location = "https://docs.google.com/spreadsheets/d/1teIkJppQ6-TebIr3SJyKtJ6Fy__jbIerq6oSZIvefRk/edit?usp=sharing",
+  pdf_location = "https://github.com/mathieu-research/mathieu-research.github.io/strayer_cv.pdf",
+  html_location = "https://github.com/mathieu-research/mathieu-research.github.io",
+  source_location = "https://github.com/mathieu-research/mathieu-research.github.io",
   output_dir = here(),
   open_files = FALSE
 )
